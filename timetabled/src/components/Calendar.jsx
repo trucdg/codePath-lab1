c17db584f06884fd5a7add2dea023d4c7118988d
Truc Duong <trucdg@bu.edu>
2023-02-23 14:07:12 -0500

    write test codes for the Calendar component

---
+++ timetabled/src/components/Calendar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+const Calendar = () => {
+  return (
+    <div className="Calendar">
+      <p>Testing the calendar component</p>
+    </div>
+  );
+};
+
+export default Calendar;
