034c3f146f45154d81d9a74ce3f040c1fff6d161
Truc Duong <trucdg@bu.edu>
2023-02-23 14:01:46 -0500

    add an h1 for the project title and h2 for the schedule description

--- timetabled/src/App.jsx
+++ timetabled/src/App.jsx
@@ -3,7 +3,11 @@ import "./App.css";
 const App = () => {
   return (
     <div classNAme="App">
-      <p>CodePath lab 1 - timetabled</p>
+      <h1> Schedule for a Productive Week </h1>
+      <h2>
+        This is a standard 1-week schedule for a busy college student who wants
+        to maintain a healthy and balance lifestyle.
+      </h2>
     </div>
   );
 };
